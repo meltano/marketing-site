31acc486befd10901d10bdf024b433e0b786f9dc
Igor Abdulović <igorabdul@gmail.com>
2023-07-12 13:02:09 +0200

    add announcement close state

--- src/components/preheader.js
+++ src/components/preheader.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-inner-declarations */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
 
@@ -24,10 +24,57 @@ const Preheader = () => {
       }
     `
   )
-
   const { preHeader } = data.allWpPage.nodes[0]
+  const { preHeaderOn } = preHeader
 
-  if (preHeader.preHeaderOn)
+  const [isVisible, setIsVisible] = useState(
+    (typeof localStorage !== 'undefined' &&
+      localStorage.getItem(`meltano.announcement`) &&
+      !localStorage.getItem(`meltano.announcement.closed`)) ||
+      false
+  )
+  const [isClosed, setIsClosed] = useState(
+    (typeof localStorage !== 'undefined' &&
+      localStorage.getItem(`meltano.announcement.closed`)) ||
+      false
+  )
+  const [isNew, setIsNew] = useState(false)
+
+  const id = encodeURIComponent(`${preHeader.preHeaderTextShort}w`)
+
+  const handleClose = () => {
+    setIsClosed(true)
+    setIsVisible(false)
+    localStorage.setItem(`meltano.announcement.closed`, JSON.stringify(true))
+  }
+
+  useEffect(() => {
+    if (!preHeaderOn) {
+      return
+    }
+
+    const viewedId = localStorage.getItem(`meltano.announcement`)
+
+    setIsNew(viewedId !== JSON.stringify(id))
+
+    if (viewedId !== JSON.stringify(id)) {
+      localStorage.setItem(`meltano.announcement`, JSON.stringify(id))
+      setIsNew(true)
+    }
+
+    if (!isClosed) {
+      localStorage.removeItem(`meltano.announcement.closed`)
+      setIsVisible(true)
+    }
+
+    if (isClosed && isNew) {
+      localStorage.setItem(`meltano.announcement`, JSON.stringify(id))
+      setIsVisible(true)
+      setIsClosed(false)
+    }
+  }, [isNew, isClosed, id, preHeaderOn])
+
+  if (isVisible)
     return (
       <>
         <Helmet>
@@ -37,7 +84,7 @@ const Preheader = () => {
           <div className="pre-header-container">
             {preHeader.preHeaderLink ? (
               <Link to={preHeader.preHeaderLink.url}>
-                <div className="pre-header-content p3">
+                <div className="pre-header-content p3a">
                   <span
                     className="show-desktop"
                     dangerouslySetInnerHTML={{
@@ -77,6 +124,29 @@ const Preheader = () => {
               </div>
             )}
           </div>
+          <button
+            type="button"
+            aria-label="Close"
+            className="close-preheader-button"
+            onClick={handleClose}
+          >
+            <svg viewBox="0 0 28 19" width="14" height="14">
+              <g>
+                <path
+                  id="Vector 1"
+                  d="M23 17.5L7 1.5"
+                  stroke="#ffffff"
+                  strokeWidth="3.1"
+                />
+                <path
+                  id="Vector 3"
+                  d="M23 1.5L7 17.5"
+                  stroke="#ffffff"
+                  strokeWidth="3.1"
+                />
+              </g>
+            </svg>
+          </button>
         </div>
       </>
     )
